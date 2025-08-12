217a40ca7c2759a32d437058270b48e7830a98e2
Nicolas Bareil <nbareil@paloaltonetworks.com>
2025-08-12 10:06:13 +0000

    evtxTools: force CargoHash

--- pkgs/evtxTools.nix
+++ pkgs/evtxTools.nix
@@ -13,7 +13,7 @@ rustPlatform.buildRustPackage rec {
       sha256 = "sha256-aa04Ia11+Ae1amc3JAtYdSWf+f/fenTt0Bny/AauaHo=";
     };
 
-    cargoSha256 = "sha256-pfTWbMWLdzA+AIZRmQebNh1vroVDxjaJjxxlm1fPdPY=";
+    cargoHash = "sha256-pfTWbMWLdzA+AIZRmQebNh1vroVDxjaJjxxlm1fPdPY=";
 
     meta = {
       description = "A cross-platform parser for the Windows XML EventLog format";
